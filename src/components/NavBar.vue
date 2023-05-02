b20e56ab2f65e88a6a6c05171c8827430246a68d
rdbcasillas <rdbcasillas11@gmail.com>
2023-05-02 12:18:22 +0530

    initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,69 @@
+<template>
+  <b-navbar
+    id="navbar"
+    fixed="top"
+    :class="{ 'navbar-scrolled': isScrolled }"
+    type="transparent"
+    variant="light"
+  >
+    <b-navbar-brand href="#"> YourName </b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#about" @click.prevent="smoothScroll('about')"
+          >About</b-nav-item
+        >
+        <b-nav-item href="#portfolio" @click.prevent="smoothScroll('portfolio')"
+          >Portfolio</b-nav-item
+        >
+        <b-nav-item href="#blog" @click.prevent="smoothScroll('blog')"
+          >Blog</b-nav-item
+        >
+        <b-nav-item href="#contact" @click.prevent="smoothScroll('contact')"
+          >Contact</b-nav-item
+        >
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      isScrolled: false,
+    };
+  },
+  methods: {
+    smoothScroll(targetId) {
+      const targetElement = document.getElementById(targetId);
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", () => {
+      this.isScrolled = window.scrollY > 0;
+    });
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.isScrolled);
+  },
+};
+</script>
+
+<style scoped>
+#navbar {
+  transition: all 0.3s;
+  padding: 1rem;
+  background: transparent;
+}
+
+.navbar-scrolled {
+  background: rgba(0, 0, 0, 0.8);
+}
+
+.b-nav-item {
+  cursor: pointer;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <b-container class="landing-page">
+      <b-row class="justify-content-center align-items-center hero-section">
+        <b-col md="6">
+          <img src="mypic.jpg" alt="Your image" class="img-fluid hero-image" />
+          <h1 class="display-4">Your Name</h1>
+          <p class="lead">
+            Bioinformatician and a Software Developer with over 10 years of
+            experience specializing in NGS pipelines, web development and data
+            visualization. Skilled at delivering interactive tools for
+            researchers, that provide novel insights in healthcare sector and
+            biomedical sciences. Adept at learning new technologies
+            autodidactically and flexible with different tech stacks. Loves
+            <a href="http://rdbcasillas.github.io">writing</a>,
+            <a href="https://open.spotify.com/show/79oICYy7ndxZIrQQWunirp"
+              >podcasting</a
+            >
+            and
+            <a
+              href="https://year-one.hyperlink.academy/courses/introduction-to-applied-rationality/54"
+              >teaching rationality</a
+            >.
+          </p>
+          <p class="lead">
+            Occassionally, I also work with people directly to help with their
+            personal and professional issues using numerous
+            <a
+              href="https://www.lesswrong.com/posts/RcZCwxFiZzE6X7nsv/what-do-we-mean-by-rationality-1"
+              >instrumental rationality</a
+            >
+            techniques and frameworks (see projects section below for an
+            overview of my public work). If you are interested in a
+            coaching/career counselling session, please schedule a spot
+            <a href="https://calendly.com/vatsalmehra/60-min-chat">here</a>.
+          </p>
+          <b-button variant="primary" href="#about">Learn More</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+};
+</script>
+
+<style scoped>
+.landing-page {
+  min-height: 100vh;
+  padding-top: 5rem;
+}
+
+.hero-section {
+  height: 100%;
+}
+
+.hero-image {
+  max-width: 300px;
+  margin-bottom: 2rem;
+  border-radius: 50%;
+}
+.lead {
+  text-align: justify !important;
+}
+</style>
